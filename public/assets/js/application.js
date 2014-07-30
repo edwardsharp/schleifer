709066a301af7ad67244d05868bf508ce343239f
edward sharp <edward@edwardsharp.net>
2014-07-30 01:47:25 -0400

    fixupz

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -5,7 +5,7 @@ ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
   $("#sock-text").append("<div class='col-md-6'><div class='panel panel-default'><div class='panel-heading'>" + data.handle + "</div><div class='panel-body'>" + data.text + "</div></div></div>");
   $("#sock-text").stop().animate({
-    scrollLeft: $('#sock-text')[0].scrollWidth
+    scrollTop: $('#sock-text')[0].scrollHeight
   }, 800);
 };
 
@@ -13,6 +13,8 @@ $("#input-form").on("submit", function(event) {
   event.preventDefault();
   var handle = $("#input-handle")[0].value;
   var text   = $("#input-text")[0].value;
-  ws.send(JSON.stringify({ handle: handle, text: text }));
-  $("#input-text")[0].value = "";
+  if(!text.empty()){
+    ws.send(JSON.stringify({ handle: handle, text: text }));
+    $("#input-text")[0].value = "";
+  }
 });
