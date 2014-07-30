43b6dbe75ff834960ecc31619d5c48b068cee7af
edward sharp <edward@edwardsharp.net>
2014-07-30 02:26:06 -0400

    fixupz

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -3,9 +3,9 @@ var uri      = scheme + window.document.location.host + "/";
 var ws       = new WebSocket(uri);
 ws.onmessage = function(message) {
   var data = JSON.parse(message.data);
-  var iframeshit = "<iframe width='1280' height='720' src='//www.youtube-nocookie.com/embed/v9AKH16--VE?rel=0' frameborder='0' allowfullscreen></iframe>";
+  var iframeshit = "<iframe src='//www.youtube-nocookie.com/embed/v9AKH16--VE?rel=0' frameborder='0' allowfullscreen></iframe>";
   //data.videoid
-  $("#sock-text").append("<div class='embed-responsive embed-responsive-16by9'>" + iframeshit + "</div>");
+  $("#sock-text").append("<div class='clr embed-responsive embed-responsive-16by9'>" + iframeshit + "</div>");
   $("#sock-text").stop().animate({
     scrollTop: $('#sock-text')[0].scrollHeight
   }, 800);
